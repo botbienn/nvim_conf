26326519471e1ceca1a7207a8ec33635805b84e5
BotBien <vubac2004@gmail.com>
2025-10-22 20:12:59 +0700

    [Feat] remove supermaven, normal lsp cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -116,7 +116,7 @@ return {
             completion = { completeopt = "menu,menuone,select" },
 
             experimental = {
-                ghost_text = true,
+                ghost_text = false,
             },
 
             snippet = {
@@ -142,16 +142,16 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
                 ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                -- ["<C-R>"] = cmp.mapping.confirm({ select = true }),
+                -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = "nvim_lsp" },
                 { name = "luasnip" }, -- For luasnip users.
+                { name = "nvim_lsp" },
                 { name = "vim-dadbod-completion" },
             }, {
-                { name = "buffer" },
+                -- { name = "buffer" },
             }),
         })
 
